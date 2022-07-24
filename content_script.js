9541a94fba18fdc6634df41b7059c1f0a8106ac2
Pi <paingthetko.pi@gmail.com>
2022-07-25 03:05:39 +0400

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,38 +1,47 @@
 var url = window.location.href;
 
 function zap(ele) {
-	var element = document.getElementById(ele);
-	if (typeof (element) != 'undefined' && element != null) {
-		element.style.visibility = "hidden";
-	}
+  var element = document.getElementById(ele);
+  if (typeof element != "undefined" && element != null) {
+    element.style.visibility = "hidden";
+  }
 }
 
 function zapimg() {
-	var images = document.getElementsByTagName('img');
-	while (images.length > 0) {
-		images[0].parentNode.removeChild(images[0]);
-	}
-	var images = document.getElementsByTagName('image');
-	while (images.length > 0) {
-		images[0].parentNode.removeChild(images[0]);
-	}
+  var images = document.getElementsByTagName("img");
+  while (images.length > 0) {
+    images[0].parentNode.removeChild(images[0]);
+  }
+  var images = document.getElementsByTagName("image");
+  while (images.length > 0) {
+    images[0].parentNode.removeChild(images[0]);
+  }
 }
 
 function mutetube() {
-	zapimg();
-	zap("related");
-	zap("items");
-	zap("comments");
-	zap("continuations");
-	zap("chips");
+  zapimg();
+  zap("related");
+  zap("items");
+  zap("comments");
+  zap("continuations");
+  zap("chips");
 }
 
 if (url.includes("youtube.com")) {
-	document.getElementById("title").innerHTML = "Minimalist Youtube<br> <br><div style='font-size:1em;font-weight: normal;'> Just search and only watch you actually need. <br>- No thumbnails <br>- No comments <br>- No recommendations<br><br> In case you want to see the video preview, hover over the thumbnail.</div>";
-	zapimg();
-	zap("contents");
-	zap("chips");
-	setInterval(function () {
-		mutetube();
-	}, 50);
+  document.getElementById("title").innerHTML =
+    "Minimalist Youtube<br> <br><div style='font-size:1em;font-weight: normal;'> Just search and only watch you actually need. <br>- No thumbnails <br>- No comments <br>- No recommendations<br><br> In case you want to see the video preview, hover over the thumbnail.</div>";
+  try {
+    zapimg();
+    zap("contents");
+    zap("chips");
+  } catch (err) {
+    console.log("Youmin : " + err.message);
+  }
+  setInterval(function () {
+    try {
+      mutetube();
+    } catch (err) {
+      console.log("Youmin : " + err.message);
+    }
+  }, 50);
 }
